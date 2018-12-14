8826ea0d1d9817151467d795cecd541e88b77e10
Shreya Kumar <shreyakumar@nbp-203-200.nbp.ruw.rutgers.edu>
2018-12-14 00:37:42 -0500

    add

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 let mylogger = document.getElementById('login');
 mylogger.addEventListener('click', function(){
-    if(mylogger.textContent == 'Log In' || mylogger.innerText == 'Log In') {
+    if(mylogger.textContent == 'Log Me In' || mylogger.innerText == 'Log Me In') {
         window.location.href = '/login';
     } else {
         window.location.href='/logout';
