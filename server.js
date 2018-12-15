2c2ebf2c283f1f56592543e0221c7a4061746d3f
Shreya Kumar <shreyakumar@nbp-211-91.nbp.ruw.rutgers.edu>
2018-12-15 15:13:54 -0500

    add

--- server.js
+++ server.js
@@ -147,6 +147,7 @@ app.post('/register', function (req, res) {
     const lname = req.body.lname;
     const ruemail= req.body.ruemail;
     const category = req.body.category;
+    const service = req.body.service;
     if (!username || !pass) {
         req.flash('registerMessage', 'Username and password are required.')
         return res.redirect('/register');
@@ -170,8 +171,8 @@ app.post('/register', function (req, res) {
             bcrypt.hash(pass, salt, function (err, hash) {
                 if (err) throw err;
                 // Add user to database with username and hash
-                const q = `INSERT INTO server(serverid, username, hash, fname, lname, ruemail, category) VALUES (null, ?, ?, ?, ?, ?, ?)`;
-                db.query(q, [username, hash, fname, lname, ruemail, category], function (err, results, fields) {
+                const q = `INSERT INTO server(serverid, username, hash, fname, lname, ruemail, category, service) VALUES (null, ?, ?, ?, ?, ?, ?, ?)`;
+                db.query(q, [username, hash, fname, lname, ruemail, category, service], function (err, results, fields) {
                     if (err) console.error(err);
                     req.flash('loginMessage', 'Account created successfully.');
                     res.redirect('/login');
