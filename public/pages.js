c4fc92116a562d7d0c489e64c9ebb0d443b319ca
Shreya Kumar <shreyakumar@Shreyas-MacBook-Pro.local>
2018-12-16 21:26:35 -0500

    add

--- public/pages.js
+++ public/pages.js
@@ -1,3 +1,13 @@
+
+let mylogger = document.getElementById('mylogger');
+mylogger.addEventListener('click', function(){
+    if(mylogger.textContent == 'Log Me In' || mylogger.innerText == 'Log Me In') {
+        window.location.href = '/login';
+    } else {
+        window.location.href='/logout';
+    }
+});
+
 let eyebrows = document.getElementById('eyebrows');
 eyebrows.addEventListener('click', function(){
     window.location.href = '/eyebrows';
@@ -48,11 +58,3 @@ logo.addEventListener('click', function(){
     window.location.href = '/';
 });
 
-let mylogger = document.getElementById('mylogger');
-mylogger.addEventListener('click', function(){
-    if(mylogger.textContent == 'Log Me In' || mylogger.innerText == 'Log Me In') {
-        window.location.href = '/login';
-    } else {
-        window.location.href='/logout';
-    }
-});
