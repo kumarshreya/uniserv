76c2db11159d02d9645029e99f8a885cae78e24e
Shreya Kumar <shreyakumar@Shreyas-MacBook-Pro.local>
2018-12-16 21:20:50 -0500

    add

--- public/pages.js
+++ public/pages.js
@@ -47,3 +47,12 @@ let logo = document.getElementById('logo');
 logo.addEventListener('click', function(){
     window.location.href = '/';
 });
+
+let mylogger = document.getElementById('mylogger');
+mylogger.addEventListener('click', function(){
+    if(mylogger.textContent == 'Log Me In' || mylogger.innerText == 'Log Me In') {
+        window.location.href = '/login';
+    } else {
+        window.location.href='/logout';
+    }
+});
